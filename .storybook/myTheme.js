167f14ff99626cec244aad8b822081471a9a3767
daipengpeng <daipengpeng@saicmotor.com>
2024-05-10 16:50:57 +0800

    feat: update docs

--- .storybook/myTheme.js
+++ .storybook/myTheme.js
@@ -2,8 +2,8 @@ import { create } from '@storybook/theming/create';
 
 export default create({
     base: 'light',
-    brandTitle: 'Yodao UI',
-    brandUrl: 'https://zhixuan.youdao.com/',
-    brandImage: 'http://shared.ydstatic.com/ead/dynamic/dynamic-template/image/ow_adimg1/logo.png',
+    brandTitle: 'Valor-mobile',
+    brandUrl: '',
+    brandImage: '',
     brandTarget: '_blank',
 });
